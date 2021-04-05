b89570147b9b4980ce2f470b68c1487250b9f2e9
Ezequiel Faiella <ezequielfaiella@gmail.com>
2021-04-05 14:35:57 -0300

    casi finalizado

--- inventario.py
+++ inventario.py
@@ -25,7 +25,13 @@ def index():
                     '''
     cursor.execute(listar_stock)
     stock_actual = cursor.fetchall()
-    return render_template('index.html', cantidades=stock_actual)
+    listar_stock2 = '''select articulos.codigo, articulos.nombre, articulos.unidad_medida, materiaprima.cantidad
+                    from articulos
+                    INNER JOIN materiaprima  on articulos.codigo = materiaprima.codigo and articulos.unidad_medida = materiaprima.unidad_medida
+                    '''
+    cursor.execute(listar_stock2)
+    mp_actual = cursor.fetchall()
+    return render_template('index.html', cantidades=stock_actual, materiasprimas=mp_actual)
 
 #  pagina de carga de nuevos articulos
 @app.route('/alta', methods=['POST'])
@@ -109,17 +115,51 @@ def salida():
             conexion.commit()
             #  mensaje de que se guardo con exito
             flash("Entrada Agregada con Éxito")
+            # devulevo a la pagina indice
             return redirect(url_for('index'))
         else:
+            #  mensaje de que no existe el articulo
             flash("Artículo Inexistente")
+            # devulevo a la pagina indice
             return redirect(url_for('index'))
 
 
 # ME QUEDE EN AGREGAR MODIFICACION Y ELIMINACION DE ARTICULOS
-@app.route('/modificar', methods=['POST'])
-def modificar():
-    continue    
+@app.route('/modificar/<codigo>')
+def datos_modificar(codigo):
+    # HAGO LA OCNSULTA en la base de datos por el valor que tengo al hacer click en modificar
+    fecha = time.strftime("%c")
+    mod_mp = '''select articulos.codigo, articulos.nombre, materiaprima.cantidad
+                    from articulos
+                    INNER JOIN materiaprima  on articulos.codigo = materiaprima.codigo and articulos.unidad_medida = materiaprima.unidad_medida
+                    where materiaprima.codigo = ?
+                    '''
+    # ejecuto la ocnsulta
+    cursor.execute(mod_mp, (codigo,))  #LOS CORCHETES ES PARA QUE TOME LITERAL EL VALOR DE LA VARIABLE PORQUE CON LA TUPLA TOMA 3 (154 COMO 1,5 Y 4) sino (codigo y ,) para q haga tupla
+    # guardo los datos en la variable
+    mp_modificar = cursor.fetchall()
+    return render_template('modificar.html', mp_edit = mp_modificar[0])    
 
+@app.route('/actualiza/<codigo>', methods=['POST'])
+def modificar(codigo):
+    if request.method == 'POST':
+        fecha = time.strftime("%c")
+        # obtengo los datos cargados en el formulario y los mando a variables
+        # articulo_cod = request.form['articulo_cod']
+        # articulo_um = request.form['articulo_um']
+        cantidad = request.form['cantidad']
+        # primero controlo que no existe, si es asi 
+        consulta = '''update materiaprima
+                        set cantidad = ?,
+                        fecha_movimientos = ?
+                    where codigo = ?
+        '''
+        cursor.execute(consulta, (cantidad, fecha, codigo))
+        # los guardo
+        conexion.commit()
+        #  mensaje de que se guardo con exito
+        flash("Entrada Modificada con Éxito")
+        return redirect(url_for('index'))
 
 # @app.roure('/borrar')
 # def eliminar():
